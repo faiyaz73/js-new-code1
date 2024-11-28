b01114785178e642545b976b3d8192045e33ebc8
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-11-28 07:01:55 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12345;
+let acountEmail = "faiyazansari15901@gmail.com"
+var accountPassword = "12345"
+var accountCity = "up"
+//accountId = 123   // not allowed
+ acountEmail = "faiyaz9032@gmail.com"
+ accountPassword = 12234;
+ accountCity = "mp"
+console.log(accountId);
+console.log(accountCity);
+console.log(accountPassword);
+console.log(acountEmail);
+console.table([accountCity,accountPassword,acountEmail])
+
+
+
