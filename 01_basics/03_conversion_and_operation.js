e6c947096e721c6a86a3e9af99e34265b5abbb34
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-11-29 07:18:04 +0000

    conversion of data type

---
+++ 01_basics/03_conversion_and_operation.js
@@ -0,0 +1,28 @@
+let score = "23acv"                    // output not a numbers 
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueNumber = Number(score)      // this is a change of string to numbers ,any numerical number in string chnage number form 
+// console.log(typeof valueNumber)
+// console.log(valueNumber)
+
+//"33" => 33
+// "333ab"=> NaN
+// true => 1 false => 0
+// undefined => NaN 
+// null => 0 
+
+
+let isloggIn = "faiyaz";
+let booleanIsloggedIn = Boolean(isloggIn)
+console.log(booleanIsloggedIn);
+
+//1 => true; 0 => false
+//"" => false
+// "faiyaz" => true
+
+
+let sumNumber = 234;
+let stringNumber = String(sumNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
