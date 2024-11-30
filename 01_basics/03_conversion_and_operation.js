5e7180d43bb8d9e700e4ea75f8b42e3fcc513092
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-11-30 07:24:39 +0000

    conversion

--- 01_basics/03_conversion_and_operation.js
+++ 01_basics/03_conversion_and_operation.js
@@ -13,16 +13,48 @@ let valueNumber = Number(score)      // this is a change of string to numbers ,a
 // null => 0 
 
 
-let isloggIn = "faiyaz";
-let booleanIsloggedIn = Boolean(isloggIn)
-console.log(booleanIsloggedIn);
+// let isloggIn = "faiyaz";
+// let booleanIsloggedIn = Boolean(isloggIn)
+// console.log(booleanIsloggedIn);
 
 //1 => true; 0 => false
 //"" => false
 // "faiyaz" => true
 
 
-let sumNumber = 234;
-let stringNumber = String(sumNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let sumNumber = 234;
+// let stringNumber = String(sumNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ****************** operations **************
+let value  = 3
+let negNum = -value
+// console.log(negNum)
+// console.log(3+3)
+// console.log(4-2)
+// console.log(4*22)
+// console.log(4**2)
+// console.log(4/2)
+// console.log(4%2)
+
+
+let str1 = "hellow"
+let str2 = " faiyaz"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2);
+// console.log(2 + 2 + "2");
+// console.log(+true);      // 1 but true type output true 
+// console.log(+"");       // 0 output
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
