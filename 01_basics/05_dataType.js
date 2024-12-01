000f7a03224970d9e9f107a2505bf5656a8e9f40
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-01 07:15:18 +0000

    data_type

---
+++ 01_basics/05_dataType.js
@@ -0,0 +1,49 @@
+//1-  primitive data type::
+// 1 string 
+//2 number
+// 3 Boolean
+// 4 null
+// 5 undefined
+// 6 Symbol
+// 7 BigInt
+
+
+// const score  = 234;
+// const scoreValue = 234.434
+//  const isLoIn = false
+//  const outsideTem = null      typeof object data type 
+//  let userEmail;
+//  const id = Symbol('123')
+//  const anotherId = Symbol('123')
+
+//  console.log(id===anotherId);
+//  const bigNumber = 23233434333443344343433434n
+
+
+
+
+
+
+
+
+
+// 2-Reference (Non-primitive)
+
+// Array,object,functions
+
+
+const hero = ["satyam","faiyaz","amit","rakesh"] // this is a array define
+let myobj = {
+    name:"faiyaz ansari",
+    rollNumber:4444,
+}
+
+console.log(typeof myobj);
+
+
+const myfunction = function num(){
+    console.log("hellow");
+
+}
+
+
