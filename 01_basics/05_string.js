c2c2ea8f01791a5a4a8c08e8eb1d08d0a9469683
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-06 06:49:27 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,35 @@
+// const name = "faiyaz ansari"
+// const repo = 40;
+// console.log(name + repo + " value")
+
+// console.log(`hellow dear sir ${name.toUpperCase()} and  my ${repo} is : `);  // String interpolation 
+// const name1 = new String("faiyaz ansari")
+// console.log(name1)
+// console.log(name1.__proto__)
+// console.log(name1.toUpperCase())
+// console.log(name1.charAt(2))
+// console.log(name1.indexOf('i'))
+//  const n = name1.slice(0,5)
+//  console.log(n)
+//  const j = "     faiyaz ansari    "
+//  const i = "akash"
+//  console.log(j)
+//  const m  = j.trim();
+//  console.log(m)
+// const rep = j.replace("fai","ama")
+// console.log(rep)
+// const inc = j.includes("ta")
+// console.log(inc)
+
+// const arr = i.split('____')
+// console.log(arr)
+
+
+const bo = "faiyaz ansari";
+console.log(bo.bold())
+
+
+
+const contentString = "Hello, world";
+
+console.log(bo.concat(contentString))
