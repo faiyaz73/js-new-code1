4a9a51f2f83c046db2390c1d4eb841cb07429982
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-11 03:13:31 +0000

    number and math

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,43 @@
+// number library 
+
+// const score = 2323
+// const score1 = 434
+// console.log(score)
+// const a = new Number(34434)
+// console.log(a)
+// const n = console.log(a.toString().length)
+// console.log(a.toFixed(2))
+
+// const  num = 23.8966
+// console.log(num.toPrecision(3))
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString('en-IN'))     // this is a comma seprated value output
+
+// __________  maths library _______
+
+
+
+// console.log(Math)
+
+// console.log(Math.abs(-233))
+// console.log(Math.pow(3,4))
+// console.log(Math.round(34.6))
+// console.log(Math.ceil(4.5))
+// console.log(Math.floor(4.5))
+// console.log(Math.min(5,23,245,234,1,56,4))
+// console.log(Math.max(5,23,245,234,1,56,4))
+// console.log(Math.random())
+// console.log(Math.random() * 10 + 1)
+
+const min  = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
