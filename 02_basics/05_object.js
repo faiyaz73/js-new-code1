fbf083d53ed6b1f005d748bdfa49e599177e85c0
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-15 14:06:49 +0000

    json

---
+++ 02_basics/05_object.js
@@ -0,0 +1,28 @@
+const course = {
+    courseName:"python",
+    price:10000,
+    courseInsructor:"faiyaz ansari"
+}
+// console.log(course.courseName)
+const{courseInsructor} = course                   // 2nd method print courseinstructor name 
+const{courseInsructor:instut} = course            // this is a distructering 
+// console.log(courseInsructor)
+// console.log(instut)                              
+
+// json file      (java script object notation)
+
+// {
+// name:"faiyaz",                                       // this is a json file(as a object)
+// rollNumber:2344,
+// course:"python"
+// }
+
+
+// [
+//     {
+//         name:"faiyaz",                                       // this is a json file(as a array form)
+        // rollNumber:2344,
+        // course:"python" 
+//     }
+// ]
+
