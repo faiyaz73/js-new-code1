648368b6996986e292a15aa668edcd75fe7f1ac5
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-15 05:39:45 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,22 @@
+// const a = ["saktiman","ironman","spiderman"]
+// const dc = ["superman","flash","batman"]
+
+// a.push(dc)                       // all value push array elements 
+// console.log(a[3])                
+
+// const b = a.concat(dc)             // all elements concatenates 
+// console.log(b)
+
+
+// const veg = [`tomato`, `onion`,`potato`,'lady finger']
+// const fruits  = [`apple`,`orange`,`grapes`]
+
+// const com = [...veg, ...fruits]           // this is a spread operation all value combind as a concatenate
+// console.log(com)
+
+const new_array = [1,2,3,4,5,[6,7,8,9],10,11,12,13,[14,15,16,17],[18,19,20,[21,22,23]]]
+// console.log(new_array)
+// console.log(new_array[3])
+// console.log(new_array[11][3][1])
+
+const new_another_array = new_array.flat
