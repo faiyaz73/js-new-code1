121043e5a6e7fc20a6fd9cac4b91f7cb141824ad
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-18 06:54:48 +0000

    arrow

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -17,8 +17,37 @@ user.username = "faiyaz"
 const arr = () => {
     let usename  = "firoj ansari"
     console.log(usename);
-    console.log(this)   
+    // console.log(this)                              // this is a empty 
 
 }
-arr()
+// arr()
+// console.log(this)                       // this is empty
+
+
+const am = () => {
+    const n = 233;
+    console.log(n)
+
+}
+
+am()
+
+const y = () => {
+    const r = "faiyaz"
+    console.log(r)
 console.log(this)
+}
+                        
+y()                       
+
+const sum =(num1,num2) => {
+return num1 + num2
+}
+const sum1 = sum(34,45)
+console.log(sum1)
+
+// const number = (no1, no2) => no1 * no2              // implicit return 
+// console.log(number(34,5))
+
+const ob = (ob1, obj2) => ({name:"faiyaz"})           // this is a object implicit 
+console.log(ob(3,5))
