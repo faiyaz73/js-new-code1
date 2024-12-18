4c55f40c9ad5f63ea3f5f8d6620d3afa8a1a61e5
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-18 17:34:19 +0000

    execution

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -1,47 +1,69 @@
 
-if(true){
-    let a = 10
-    const b = 20
-    var c  = 30
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c  = 30
     
-}
-
-// console.log(a)
-// console.log(b)
-// console.log(c)
+// }
 
 
-function one(){
-    const username = "faiyaz ansari"
 
-    function two(){
-        const webside = "youtube"
-        console.log(username)
-    }
-    // console.log(webside)
 
-    two()
-}
-// one()
 
-if(true){
-    const username = "aman"
-    if(username === "aman"){                                // this is a closer
-        const webside = " youtube "
-        // console.log(username + webside)                     
-    }
-    // console.log(webside);
-}
+
+
+
+// function one(){
+//     const username = "faiyaz ansari"
+
+//     function two(){
+//         const webside = "youtube"
+//         console.log(username)
+//     }
+//     // console.log(webside)
+
+//     two()
+// }
+// // one()
+
+
+
+
+// function nishi(){
+//     const username  = "nishita pal"
+//     function one(){
+//         const web = "facebook"
+//         console.log(web)
+//     }
+//     one()
+//     console.log(username)
+// }
+// nishi()// one()
+
+// if(true){
+//     const username = "aman"
+//     if(username === "aman"){                                // this is a closer
+//         const webside = " youtube "
+//         // console.log(username + webside)                     
+//     }
+//     // console.log(webside);
+// }
 // console.log(username);
 
-// ++++++++++++++++++++++++++++++++ interesting  +++++++++++++++++++++++++++
 
-console.log(addone(34))
-function addone(num){
-return num + 1
-}
 
-console.log(sumtwo(3))               // this is a error beacuse this first initilization so error output            
-const sumtwo = function(num){
-    return num + 2
-}
+
+
+
+
+// // ++++++++++++++++++++++++++++++++ interesting  +++++++++++++++++++++++++++
+
+// console.log(addone(34))
+// function addone(num){
+// return num + 1
+// }
+
+// console.log(sumtwo(3))               // this is a error beacuse this first initilization so error output            
+// const sumtwo = function(num){
+//     return num + 2
+// }
---
+++ 03_basic/05_excecution_context.js
@@ -0,0 +1,17 @@
+// javascript  execution context 
+
+// {} => global execution context => this variable ke andar global execution ko rkha diya jata h. browser ke 
+// andar me this ki value window object aati h 
+// java script is  A single-threaded language is one that can execute only one task at a time. 
+
+
+// 1 global execution context 
+// 2 function execution context 
+// 3 eval execution context          (mangoes documentation me work krta h
+// )
+
+// {} two phase me java script ka code run hota h 
+// 1 - memory creation phase  ( es me variable , any declairation location locate krta  h ye)
+// 2- execution phase (any calculation execution)
+
+
