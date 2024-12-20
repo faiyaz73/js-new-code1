1ac7cd45aa1ed335f2920b18ca2522385f82370c
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-19 23:48:32 -0800

    Add files via upload

---
+++ 03_basic/one.js
@@ -0,0 +1,18 @@
+// if
+const isUserLoggIn = true
+if(isUserLoggIn){
+  console.log("User is logged in")
+}else{
+  console.log("User is not logged in")
+}
+
+
+
+if(!3 == 2){
+  console.log("Condition is true")
+}else{
+  console.log("Condition is false")
+}
+// operators 
+
+// >, < , <=, >=, ==, !=, == , === , !==  all is a comparision operators 
