95afb6586316f998f4320dc54fdfcd535eb006a3
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2025-02-24 03:13:02 -0500

    Add files via upload

---
+++ 02_basics/one.js
@@ -0,0 +1,53 @@
+// // if
+// const isUserLoggIn = true
+// if(isUserLoggIn){
+//   console.log("User is logged in")
+// }else{
+//   console.log("User is not logged in")
+// }
+
+
+
+// if(!3 == 2){
+//   console.log("Condition is true")
+// }else{
+//   console.log("Condition is false")
+// }
+// operators 
+
+// >, < , <=, >=, ==, !=, == , === , !==  all is a comparision operators 
+
+
+
+// const score = 200
+// if(score > 100){
+// const result = "You are a winner"
+// console.log(`check the winner yes ya no ${result}`)
+// }
+
+
+const balance = 1000
+// if(balance > 500) console.log("you are go to market"), console.log("you are not go to market dear ");
+
+// if(balance < 500){
+//   console.log("less than 500")
+// }else if(balance < 750){
+//   console.log("less than 750")
+// }else if(balance < 900){
+//   console.log("less than 900")
+// }else{
+//   console.log("this is  value is 1200")
+// }
+
+
+const userLoggdIn = true
+const debidCard = true
+const LoggedInFromGoogle = false
+const LoggedInFromEmail = true
+
+if(userLoggdIn && debidCard && 2==2){
+  console.log("user is logged in and debid card is active")
+}
+if(LoggedInFromGoogle || LoggedInFromEmail){
+  console.log("user is logged in from google or email")
+}
